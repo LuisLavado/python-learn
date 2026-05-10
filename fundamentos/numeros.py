5e971e35b3f8f1cd425e82c1fa1f339043a3c808
Luis Lavado Llaro <luisito.ingenermax@gmail.com>
2026-05-10 18:06:48 -0500

    Fundamentos de Python

---
+++ fundamentos/numeros.py
@@ -0,0 +1,41 @@
+x = 1
+y = 2.5
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+negativo_entero = -1
+negativo_flotante = -2.5
+print(negativo_entero)
+print(negativo_flotante)
+
+imaginario = -5 -1j -2j
+print(imaginario)
+
+xf = float(x)
+print(type(x))
+print(type(xf))
+print(xf)
+
+ye = int(y)
+print(type(y))
+print(type(ye))
+print(ye)
+
+# Enteros y Flotantes a Complejos
+entero = 5
+enteroComplejo = complex(entero)
+print(type(enteroComplejo))
+print(enteroComplejo)
+
+flotante = 5.5
+flotanteComplejo = complex(flotante)
+print(type(flotanteComplejo))
+print(flotanteComplejo)
+
+
+import random
+
+print(random.randrange(1, 10)) # Output: Num aleatoria entre 1 y 9
