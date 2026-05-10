80ba5f5bff17cd11684a8dbc0b6f5756c189563c
Luis Lavado Llaro <luisito.ingenermax@gmail.com>
2026-05-10 17:49:27 -0500

    Tipos de datos

---
+++ tipos-datos.py
@@ -0,0 +1,46 @@
+#Strings
+
+comillasSimples = 'Simples'
+comillasDobles = "Dobles"
+comillasTriples = '''Triples'''
+comillasDoblesTriples = """Comillas Dobles Triples"""
+
+print(comillasSimples)
+print(comillasDobles)
+print(comillasTriples)
+print(comillasDoblesTriples)
+
+#Números
+a=1
+b=3.14 #Num Flotante
+c=5+2j #Num Complejos (imaginarios)
+
+print(a)
+print(b)
+print(c)
+
+#Listas (Mutables)
+lista = [0,1,2,3,4,5]
+print(lista)
+
+#tupla (Inmutables)
+tupla=("a","b","c")
+print(tupla)
+
+#Diccionario
+diccionario={
+    "Nombre":"Luis",
+    "Edad":34,
+    "Ciudad":"Perú"
+}
+print(diccionario)
+
+#Conjuntos (sets)
+conjunto = {1,1,2,4,2} # Outpot: {1,2,4}
+print(conjunto)
+
+#Booleanos
+booleanoVerdadero=True
+booleanoFalso=False
+print(booleanoVerdadero)
+print(booleanoFalso)
